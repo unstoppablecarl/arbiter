04d9e10d10a99006f7b59d54eb272811d4920f0e
Carl Olsen <unstoppablecarlolsen@gmail.com>
2017-08-14 10:35:48 -0400

    default config file cleanup

--- config/arbiter.php
+++ config/arbiter.php
@@ -1,22 +1,38 @@
 <?php
 
-use UnstoppableCarl\Arbiter\TargetSelfOverrides;
 use UnstoppableCarl\Arbiter\UserAuthority;
 
 return [
 
     'user_authority' => [
-        'implementation_class'   => UserAuthority::class,
+
+        /**
+         * Abilities Primary roles can perform on other Primary Roles
+         */
         'primary_role_abilities' => [
 
-            /* Example
+            /* Example Values
 
-            'primary_role_1' => [
-//            'ability_foo' => ['primary_role_1', 'primary_role_2'],
+            'role_1' => [
+                'ability_a' => ['role_1', 'role_2'],
+                'ability_b' => ['role_1', 'role_2'],
             ],
 
-            'primary_role_2' => [
-//            'ability_bar' => ['primary_role_2'],
+            'role_2' => [
+                'ability_b' => ['role_2'],
+            ],
+
+            */
+
+            /* Boilerplate with built in keys
+
+            'admin' => [
+                UserAuthority::CAN_CHANGE_PRIMARY_ROLE_OF_USERS_TO                => ['admin'],
+                UserAuthority::CAN_CHANGE_PRIMARY_ROLE_OF_USERS_WITH_PRIMARY_ROLE => [],
+                UserAuthority::CAN_VIEW_USERS_WITH_PRIMARY_ROLE                   => [],
+                UserAuthority::CAN_CREATE_USERS_WITH_PRIMARY_ROLE                 => [],
+                UserAuthority::CAN_UPDATE_USERS_WITH_PRIMARY_ROLE                 => [],
+                UserAuthority::CAN_DELETE_USERS_WITH_PRIMARY_ROLE                 => [],
             ],
 
             */
@@ -24,11 +40,15 @@ return [
     ],
 
     'target_self_overrides' => [
-        'implementation_class' => TargetSelfOverrides::class,
-        'ability_overrides'    => [
-            /* Example
+
+        /**
+         * Overrides the returned value of an ability check when $target is the currently logged in user.
+         */
+        'ability_overrides' => [
+            /* Example Values
 
                 'delete' => false,
+                'view'   => true,
 
             */
         ],
